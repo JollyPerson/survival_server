ae782f9a477a258fc6d0811fc8894d124aad5120
JollyPerson <jollyperson_real@Outlook.com>
2021-08-06 00:19:39 +0100

    Add project files.

---
+++ Survival_Game_Server/NetworkReceive.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KaymakNetwork;
+
+namespace Survival_Game_Server
+{
+    internal enum ClientPackets
+    {
+        Ping = 1,
+    }
+
+    internal static class NetworkReceive
+    {
+        internal static void PacketRouter()
+        {
+            NetworkConfig.socket.PacketId[(int)ClientPackets.Ping] = PacketPing;
+        }
+
+        private static void PacketPing(int connectionID, ref byte[] data)
+        {
+            ByteBuffer buffer = new ByteBuffer(data);
+            string msg = buffer.ReadString();
+            Console.WriteLine(msg);
+            GameManager.CreatePlayer(connectionID);
+            buffer.Dispose();
+        }
+    }
+}
---
+++ Survival_Game_Server/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+
+namespace Survival_Game_Server
+{
+    internal class Program
+    {
+        private static Thread threadConsole;
+
+        private static void Main(string[] args)
+        {
+            threadConsole = new Thread(new ThreadStart(ConsoleThread));
+            threadConsole.Start();
+            NetworkConfig.InitNetwork();
+            NetworkConfig.socket.StartListening(5555, 4, 0);
+            Console.WriteLine("Network Intialised.");
+        }
+
+        private static void ConsoleThread()
+        {
+            Console.ReadLine();
+        }
+    }
+}
